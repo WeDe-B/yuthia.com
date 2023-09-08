c6b9b89c8e83cd9c185867656516f4602002f29b
Bikram Biswas <biswasbikramcst@gmail.com>
2023-09-08 14:43:59 +0530

    newupdates

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import logo from './y_logo512.png';
 import './App.css';
-import Lottie from "lottie-react";
-import animationData from "./website-building-of-shopping-sale.json";
-import { useRef } from 'react';
+import ComingSoon from './components/ComingSoon';
 
 function App() {
-  const screnAnimation = useRef();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        < h1> We are Coming Soon</h1> 
-        <Lottie  animationData={animationData}/>
-       
-      </header>
+        <img src={logo} className="App-logo" alt="logo" height={400}/>
+        <ComingSoon/>
       
     </div>
   );
